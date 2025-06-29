aa94f3a573d77d681ca6e4f261dd8800258a624b
Kn0ax <me@kn0ax.com>
2025-06-29 14:31:01 +0200

    yt chat detection wip

--- Moblin/Various/Model/Model.swift
+++ Moblin/Various/Model/Model.swift
@@ -223,6 +223,7 @@ class CreateStreamWizard: ObservableObject {
     @Published var twitchChannelId = ""
     @Published var kickChannelName = ""
     @Published var youTubeVideoId = ""
+    @Published var youTubeHandle = ""
     @Published var afreecaTvChannelName = ""
     @Published var afreecaTvStreamId = ""
     @Published var obsAddress = ""
